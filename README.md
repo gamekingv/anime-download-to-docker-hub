ee138a2cda5d6f416ce176fd4f83dd8f59a6555c
gamekingv <793470218@qq.com>
2021-05-11 11:26:52 +0800

    Initialize

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# anime-download-to-docker-hub
+<h1 align="center">offline-download-to-docker-hub</h1>
+
+> 离线下载各种资源并上传到Docker网盘，需要先在secrets中添加URL、USERNAME、PASSWORD变量，所有资源默认上传到/Offline目录下。
+
+> 每个下载任务最多只能运行6小时，超过会自动被终止。
+
+> （已改为并行下载）任务会串行下载，如新任务执行时检测到已有任务在进行，将会终止，并写入mongodb中进，在进行中任务完成时，会自动触发队列中的任务执行。需要在secrets中添加有触发workflow权限的TOKEN变量，以及DB_HOST、DB_USERNAME、DB_PASSWORD以连接mongodb。
+
+## 百度网盘
+在baidu-list.txt添加网盘文件路径即可，例如/folder/files.mp4，也可以是目录。需要先在secrets中添加BDUSS。
+
+## 解压下载
+在decompression-list.txt添加下载链接即可，可以是Aria2支持的所有下载链接，下载后会自动解压所有rar、zip压缩包，之后再上传。支持exhentai的种子链接，需要先在secrets中添加EX_COOKIES。
+
+## 普通离线下载
+在list.txt添加下载链接即可，可以是Aria2支持的所有下载链接，下载完成后会自动上传。
+
+## Google Drive下载
+```
+node fetch.js "API_KEY"
+```
+使用上面的命令生成下载链接列表，将生成的文件内容复制到google-drive-list.json后执行actions则可自动下载并上传。
+
+其中fetch-google-drive-list.js中变量root_folder_id为需要遍历的文件夹id。API_KEY为Google Drive API密钥，需要到 [Google API Console](https://console.cloud.google.com/apis/dashboard) 的库中开通Google Drive API，并在凭据中生成API密钥。
---
+++ analyze.js
@@ -0,0 +1,126 @@
+const fs = require('fs').promises;
+const child_process = require('child_process');
+const { promisify } = require('util');
+const exec = promisify(child_process.exec);
+const got = require('got');
+
+const {
+  GITHUB_REPOSITORY: repository,
+  QUEUE_DISPATCH_TOKEN: token
+} = process.env;
+
+const client = got.extend({
+  headers: {
+    'User-Agent': 'Github Actions'
+  },
+  timeout: 10000,
+  responseType: 'json'
+});
+
+function processOutput(output) {
+  const maxSize = 10 * 1024 * 1024 * 1024;
+  const singleFileMaxSize = 12 * 1024 * 1024 * 1024;
+  const [header, result] = output.split('\n===+===========================================================================\n');
+  const hash = header.match(/Info Hash:\s*(.*)/)[1];
+  const list = result.split('\n---+---------------------------------------------------------------------------\n');
+  const filterResult = list.filter(item => !/_____padding_file_\d+_/.test(item));
+  filterResult.pop();
+  const matchResult = filterResult.map(item => {
+    const [, index, name, size] = item.match(/^\s*(\d+)\|(.*)\n\s*\|[^(]+ \(([^)]+)\)$/);
+    return { index: Number(index), name, size: Number(size.replace(/,/g, '')) };
+  });
+  const queue = [];
+  let totalSizeTemp = 0;
+  let taskTemp = [];
+  const bigFiles = [];
+  matchResult.forEach((item, index) => {
+    if (item.size > singleFileMaxSize) {
+      bigFiles.push(`${item.name}：${(item.size / (1024 * 1024 * 1024)).toFixed(2)}GB`);
+      return;
+    }
+    totalSizeTemp += item.size;
+    if (totalSizeTemp >= maxSize) {
+      queue.push(taskTemp);
+      taskTemp = [];
+      totalSizeTemp = item.size;
+    }
+    taskTemp.push(item.index);
+    if (index === matchResult.length - 1) queue.push(taskTemp);
+  });
+  const taskList = queue.map(files => {
+    const list = files.reduce((result, file, index) => {
+      if (file === files[index + 1] - 1) {
+        if (result.charAt(result.length - 1) === ',' || result.length === 0) return result += `${file}`;
+        else if (result.charAt(result.length - 1) !== '-') return result += '-';
+        else return result;
+      }
+      else return result += `${file},`;
+    }, '');
+    return `magnet:?xt=urn:btih:${hash}\r\n  select-file=${list.slice(0, -1)}`;
+  });
+  if (bigFiles.length > 0) {
+    console.log('以下文件因过大而忽略：');
+    console.log(`magnet:?xt=urn:btih:${hash}`);
+  }
+  bigFiles.forEach(file => console.log(file));
+  return taskList;
+}
+
+async function sendToDownload(remote, local) {
+  const content = Buffer.from(local).toString('base64'),
+    configLink = remote,
+    body = {
+      message: '大文件种子下载推送',
+      content
+    },
+    headers = {
+      'Authorization': `token ${token}`,
+      'User-Agent': 'Github Actions'
+    };
+  if (!content) return 'empty';
+  const response = await client.get(configLink, {
+    headers
+  });
+  body.sha = response.body.sha;
+  await client.put(configLink, {
+    headers,
+    json: body
+  });
+}
+
+(async () => {
+  try {
+    const files = await fs.readdir('./');
+    const torrents = files.filter((item) => /\.torrent$/.test(item));
+    const tasks = [];
+    for (const torrent of torrents) {
+      const { stdout: output, stderr } = await exec(`aria2c -S "${torrent}"`);
+      if (stderr) throw stderr;
+      if (output) {
+        const list = processOutput(output);
+        tasks.push(...list);
+      }
+      else '命令无输出';
+    }
+    for (const task of tasks) {
+      try {
+        await sendToDownload(`https://api.github.com/repos/${repository}/contents/list.txt`, task);
+        console.log('');
+        console.log(task);
+        console.log('任务发送成功');
+        await new Promise(res => setTimeout(() => res(), 30000));
+      }
+      catch (error) {
+        console.log('');
+        console.log(task);
+        console.log('任务发送失败：');
+        console.log(error);
+        if (error.response && error.response.body) console.log(error.response.body);
+      }
+    }
+  }
+  catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+})();
